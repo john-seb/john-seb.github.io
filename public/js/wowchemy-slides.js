2b68523046deaec8365d06f49db36c46166fac31
john-seb <2johnsebastian@gmail.com>
2021-01-29 06:31:47 +0530

    rm

--- public/js/wowchemy-slides.js
+++ public/js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = {highlight_style: "dracula", theme: "black"};
 
-  // ns-hugo:C:\Users\MACHINE\AppData\Local\Temp\hugo_cache\modules\filecache\modules\pkg\mod\github.com\wowchemy\wowchemy-hugo-modules\wowchemy@v0.0.0-20210123180413-7dbc1f73edfd\assets\js\wowchemy-utils.js
+  // ns-hugo:C:\Users\Machine\AppData\Local\Temp\hugo_cache\modules\filecache\modules\pkg\mod\github.com\wowchemy\wowchemy-hugo-modules\wowchemy@v0.0.0-20210123180413-7dbc1f73edfd\assets\js\wowchemy-utils.js
   function fixMermaid() {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
