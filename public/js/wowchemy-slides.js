8d573df3a65df44fe2ef7ee44805016b378dafde
john-seb <2johnsebastian@gmail.com>
2023-05-29 01:00:52 +0200

    new machinie

--- public/js/wowchemy-slides.js
+++ public/js/wowchemy-slides.js
@@ -1,8 +1,8 @@
 (() => {
   // ns-params:@params
-  var slides = {highlight_style: "dracula", theme: "black"};
+  var slides = { highlight_style: "dracula", theme: "black" };
 
-  // ns-hugo:C:\Users\Machine\AppData\Local\Temp\hugo_cache\modules\filecache\modules\pkg\mod\github.com\wowchemy\wowchemy-hugo-modules\wowchemy@v0.0.0-20210123180413-7dbc1f73edfd\assets\js\wowchemy-utils.js
+  // ns-hugo:C:\Users\john_\AppData\Local\Temp\hugo_cache\modules\filecache\modules\pkg\mod\github.com\wowchemy\wowchemy-hugo-modules\wowchemy@v0.0.0-20210123180413-7dbc1f73edfd\assets\js\wowchemy-utils.js
   function fixMermaid() {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
@@ -15,7 +15,7 @@
     }
   }
 
-  // js/wowchemy-slides.js
+  // <stdin>
   var enabledPlugins = [RevealMarkdown, RevealHighlight, RevealSearch, RevealNotes, RevealMath, RevealZoom];
   var isObject = function(o) {
     return o === Object(o) && !isArray(o) && typeof o !== "function";
@@ -61,7 +61,16 @@
       var background = mslide.slideBackgroundElement;
       var currentHorizontalIndex = Reveal.getState()["indexh"];
       var diagramSlideIndex = Reveal.getIndices(mslide)["h"];
-      if (!diag.hasAttribute("data-processed") && background.hasAttribute("data-loaded") && background.style.display === "block" && diagramSlideIndex - currentHorizontalIndex <= 1)
+      if (
+        // find slides with non-rendered mermaid tags
+        // these will not have the attribute data-processed
+        !diag.hasAttribute("data-processed") && // check also that reveal slide is already loaded
+        // reveal slides seem to be lazily loaded
+        // things could be easier if reveal had a slide-loaded event
+        background.hasAttribute("data-loaded") && // loaded slides must also have the display attribute set to block
+        background.style.display === "block" && // render diagrams that are 1 slide away
+        diagramSlideIndex - currentHorizontalIndex <= 1
+      )
         return mslide;
     }
     return null;
